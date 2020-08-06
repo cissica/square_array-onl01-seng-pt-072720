e40545c74411ba7b59ab68642f5563e69262029d
Cissica Gonzalez Martinez <cecemartinez@hotmail.com>
2020-08-06 06:52:58 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,6 +2,6 @@ def square_array(numbers)
   new_numbers = []
   numbers.each do |array|
    square_array = array**4
+   return new_numbers.push(square_array)
 end
-return new_numbers.push(square_array)
 end
